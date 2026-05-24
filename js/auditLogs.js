b6fe75cbf9994607d524cb443b9267c6e5f8172c
RawanMohamed22 <rawanfathy333@gmail.com>
2026-05-25 02:59:20 +0300

    audit logs page

---
+++ js/auditLogs.js
@@ -0,0 +1,36 @@
+// modals variables
+const openModalBtns = document.querySelectorAll(".open-modal")
+const overlay = document.querySelector(".overlay")
+// end modals variables
+
+// modals logic
+openModalBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const modalId = btn.getAttribute("data-modal");
+        const targetModal = document.getElementById(modalId);
+        if (targetModal) {
+            targetModal.classList.remove("hidden");
+            overlay.classList.remove("hidden");
+        }
+    });
+});
+
+document.querySelectorAll(".close-btn, .modal-close").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const parentModal = btn.closest("[id^='modal']");
+        if (parentModal) {
+            parentModal.classList.add("hidden");
+            overlay.classList.add("hidden");
+        }
+    });
+});
+
+overlay.addEventListener("click", () => {
+    document.querySelectorAll("[id^='modal']").forEach((modal) => {
+        modal.classList.add("hidden");
+    });
+    overlay.classList.add("hidden");
+});
+// end modals logic
